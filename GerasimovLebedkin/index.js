d1d1e6f60e70459d1e43900c9f16731bcf2520b3
arri1 <ewws@mail.ru>
2023-02-14 13:56:30 +0300

    added prisma

--- GerasimovLebedkin/index.js
+++ GerasimovLebedkin/index.js
@@ -24,7 +24,7 @@ const resolvers = {
   }
 };
 
-export const schema = makeExecutableSchema({
+const schema = makeExecutableSchema({
   resolvers,
   typeDefs,
 });
@@ -35,3 +35,26 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000);
+
+
+async function main() {
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //   },
+  // })
+
+  const allUsers = await prisma.user.findMany({})
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
