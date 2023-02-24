81974ab609903e9f2cb594beb14793bc8f61e507
Witawe <texys@list.ru>
2023-02-24 17:17:43 +0900

    added popup

--- InnokentievLukovtsev/test-front/src/App.js
+++ InnokentievLukovtsev/test-front/src/App.js
@@ -1,5 +1,7 @@
 import { useQuery, gql, useMutation } from "@apollo/client";
 import React, { useState } from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 const FIND_MANY_USER = gql`
   query FindManyUser {
@@ -24,10 +26,6 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   const { loading, error, data, refetch } = useQuery(FIND_MANY_USER);
   const [
     createUser,
@@ -50,45 +48,53 @@ const App = () => {
         background: "grey"
       }}
     >
-      <button>Register</button>
+      <Popup trigger={<button className="button"> Register </button>} position="right center">
+        <div>
+          <input placeholder={"Email"} style={{ marginTop: 10 }} value={email}
+                 onChange={(e) => setEmail(e.target.value)} />
+          <input placeholder={"Name"} value={name} onChange={e => setName(e.target.value)} style={{ marginTop: 10 }} />
+          <input placeholder={"Password"} type={"password"}
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 style={{ marginTop: 10 }}
+          />
+          <button
+            style={{ marginTop: 10, width: 100, height: 30 }}
+            onClick={() => {
+              createUser({
+                variables: {
+                  data: {
+                    email,
+                    name,
+                    password
+                  }
+                }
+              })
+                .then((data) => {
+                  console.log(data);
+                  refetch();
+                  alert("user create");
+                })
+                .catch((e) => {
+                  alert(e.message);
+                });
+            }}
+          >Send
+          </button>
+        </div>
+      </Popup>
+
+      <Popup trigger={<button style={{marginTop: 10}}> List of registered users </button>} position="right center">
+        <div>
+          {!loading &&
+            data?.findManyUser?.map((item) => {
+              return <div key={item.id}>{item.email}</div>;
+            })}
+        </div>
+      </Popup>
+
       <button style={{ marginTop: 10 }}>Sign in</button>
-      <input placeholder={"Email"} style={{ marginTop: 10 }} value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input placeholder={"Name"} value={name} onChange={e => setName(e.target.value)} style={{ marginTop: 10 }} />
-      <input placeholder={"Password"} type={"password"}
-             value={password}
-             onChange={(e) => setPassword(e.target.value)}
-             style={{ marginTop: 10 }}
-      />
-      <button
-        style={{ marginTop: 10, width: 100, height: 30 }}
-        onClick={() => {
-          createUser({
-            variables: {
-              data: {
-                email,
-                name,
-                password
-              }
-            }
-          })
-            .then((data) => {
-              console.log(data);
-              refetch();
-              alert("user create");
-            })
-            .catch((e) => {
-              alert(e.message);
-            });
-        }}
-      >Send
-      </button>
-      <div style={{ marginTop: 10 }}>List of registered users</div>
-      <div style={{ marginTop: 20 }}>
-        {!loading &&
-          data?.findManyUser?.map((item) => {
-            return <div key={item.id}>{item.email}</div>;
-          })}
-      </div>
+
     </div>
   );
 };
