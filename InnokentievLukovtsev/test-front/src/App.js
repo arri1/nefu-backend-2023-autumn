375084b56d49bfe8345dee49ff009dfab202671a
Witawe <texys@list.ru>
2023-02-25 12:37:08 +0900

    added modal window

--- InnokentievLukovtsev/test-front/src/App.js
+++ InnokentievLukovtsev/test-front/src/App.js
@@ -1,7 +1,7 @@
 import { useQuery, gql, useMutation } from "@apollo/client";
 import React, { useState } from "react";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
+import Modal from "./modal/Modal";
+import "./styles.css";
 
 const FIND_MANY_USER = gql`
   query FindManyUser {
@@ -26,77 +26,97 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
+  const [RegActive, setRegActive] = useState(false);
+  const [ListActive, setListActive] = useState(false);
+  const [SignActive, setSignActive] = useState(false);
+
   const { loading, error, data, refetch } = useQuery(FIND_MANY_USER);
-  const [
-    createUser,
-    {
-      data: dataCreateUser,
-      loading: loadingCreateUser,
-      error: errorCreateUser
-    }
-  ] = useMutation(CREATE_ONE_USER);
+  const [createUser, {
+    data: dataCreateUser, loading: loadingCreateUser, error: errorCreateUser
+  }] = useMutation(CREATE_ONE_USER);
   console.log(data);
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        flexDirection: "column",
-        height: "100vh",
-        background: "grey"
-      }}
-    >
-      <Popup trigger={<button className="button"> Register </button>} position="right center">
-        <div>
-          <input placeholder={"Email"} style={{ marginTop: 10 }} value={email}
-                 onChange={(e) => setEmail(e.target.value)} />
-          <input placeholder={"Name"} value={name} onChange={e => setName(e.target.value)} style={{ marginTop: 10 }} />
-          <input placeholder={"Password"} type={"password"}
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)}
-                 style={{ marginTop: 10 }}
-          />
-          <button
-            style={{ marginTop: 10, width: 100, height: 30 }}
-            onClick={() => {
-              createUser({
-                variables: {
-                  data: {
-                    email,
-                    name,
-                    password
-                  }
+  return (<div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      flexDirection: "column",
+      height: "100vh",
+      background: "white"
+    }}
+  >
+
+    <button className={"open-btn"} style={{ marginTop: 10 }} onClick={() => setRegActive(true)}> Registration</button>
+
+    <Modal active={RegActive} setActive={setRegActive}>
+      <div style={{
+        display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", background: "grey"
+      }}>
+        <input placeholder={"Email"} style={{ marginTop: 10 }} value={email}
+               onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder={"Name"} value={name} onChange={e => setName(e.target.value)} style={{ marginTop: 10 }} />
+        <input placeholder={"Password"} type={"password"}
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               style={{ marginTop: 10 }}
+        />
+        <button
+          style={{ marginTop: 10, width: 100, height: 30, marginBottom: 10 }}
+          onClick={() => {
+            createUser({
+              variables: {
+                data: {
+                  email, name, password
                 }
+              }
+            })
+              .then((data) => {
+                console.log(data);
+                refetch();
+                alert("user create");
               })
-                .then((data) => {
-                  console.log(data);
-                  refetch();
-                  alert("user create");
-                })
-                .catch((e) => {
-                  alert(e.message);
-                });
-            }}
-          >Send
-          </button>
-        </div>
-      </Popup>
+              .catch((e) => {
+                alert(e.message);
+              });
+          }}
+        >Send
+        </button>
+      </div>
+    </Modal>
 
-      <Popup trigger={<button style={{marginTop: 10}}> List of registered users </button>} position="right center">
-        <div>
-          {!loading &&
-            data?.findManyUser?.map((item) => {
-              return <div key={item.id}>{item.email}</div>;
-            })}
-        </div>
-      </Popup>
+    <button className={"open-btn"} style={{ marginTop: 10 }} onClick={() => setListActive(true)}> List of registered users </button>
 
-      <button style={{ marginTop: 10 }}>Sign in</button>
+    <Modal active={ListActive} setActive={setListActive}>
+      <div style={{
+        display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", background: "grey"
+      }}>
+        {!loading && data?.findManyUser?.map((item) => {
+          return <div style={{
+            marginTop: 10, marginBottom: 10
+          }}
+                      key={item.id}>{item.email}</div>;
+        })}
+      </div>
+    </Modal>
 
-    </div>
-  );
+    <button className={"open-btn"} style={{ marginTop: 10 }} onClick={() => setSignActive(true)}>Sign in</button>
+
+    <Modal active={SignActive} setActive={setSignActive}>
+      <div style={{
+        display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", background: "grey"
+      }}>
+        <input placeholder={"Email"} style={{ marginTop: 10 }} value={email}
+               onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder={"Password"} type={"password"}
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               style={{ marginTop: 10 }}
+        />
+        <button style={{ marginTop: 10, marginBottom: 10 }}>Send</button>
+      </div>
+    </Modal>
+  </div>);
 };
 
 export default App;
