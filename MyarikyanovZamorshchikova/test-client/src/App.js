91d20ffdaae90ec9de2168b81f92fa8c660c6fcb
Arancreat <arancreat@gmail.com>
2023-02-28 15:07:40 +0900

    paljs/reactjs

---
+++ MyarikyanovZamorshchikova/test-client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
