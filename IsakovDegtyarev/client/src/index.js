d03fdae50716ada9b43969f64a89006cb382ce2a
Anatoliy1999 <anatoliy.isakov.1999@gmail.com>
2023-03-14 16:22:26 +0900

    commit

--- IsakovDegtyarev/client/src/index.js
+++ IsakovDegtyarev/client/src/index.js
@@ -3,21 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './pages/header/Header';
 import Main from './pages/main/Main';
-import Footer from './pages/footer/Foote';
+import Footer from './pages/footer/Footer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='container'>
-    <Header />
-    <Main />
-    <Footer />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
