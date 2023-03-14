3395670f5fd4ede79c399da100ea6f81211fe91d
aitalyy <aitalyy@gmail.com>
2023-03-14 18:55:55 +0900

    add main page

--- EgorovFedorov/client/src/App.js
+++ EgorovFedorov/client/src/App.js
@@ -1,5 +1,6 @@
 import { useQuery, gql, useMutation } from "@apollo/client";
 import { useState } from "react";
+import './App.css';
 
 const FIND_MANY_USER = gql`
   query FindManyUser {
@@ -11,11 +12,17 @@ const FIND_MANY_USER = gql`
   }
 `;
 const CREATE_ONE_USER = gql`
-  mutation CreateOneUser($data: UserCreateInput!) {
-    createOneUser(data: $data) {
+  mutation SignupUser($data: UserCreateInput!) {
+  signupUser(data: $data) {
+    token
+    user {
       id
+      email
+      password
     }
   }
+}
+  
 `;
 
 const App = () => {
@@ -41,47 +48,64 @@ const App = () => {
         width: "100%",
         flexDirection: "column",
         height: "100vh",
+
       }}
     >
-      <p>Почта:</p>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} />
 
-      <p>Пароль:</p>
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ marginTop: 10 }}
-      />
-      <button
-        style={{ marginTop: 10, width: 100, height: 30 }}
-        onClick={() => {
-          createUser({
-            variables: {
-              data: {
-                email,
-                password,
+      <div className="mainbar">
+        {/*<div className="logo">АБЭ</div>*/}
+        <p style={{fontWeight: "500"}}>Вход в соц. сети</p>
+        <input
+          className="textbox"
+          placeholder="Почта"
+          value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          className="textbox"
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          // style={{ marginTop: 10 }}
+        />
+        <button
+          className="textbox butIn"
+          // style={{ marginTop: 10, width: 100, height: 30 }}
+          onClick={() => {
+            createUser({
+              variables: {
+                data: {
+                  email,
+                  password,
+                },
               },
-            },
-          })
-            .then((data) => {
-              console.log(data);
-              refetch();
-              alert("user create");
             })
-            .catch((e) => {
-              alert(e.message);
-            });
-        }}
-      >
-        SignIn
-      </button>
-      <p style={{paddingTop: 40}}>Пользователи</p>
-      <div style={{ marginTop: 20}}>
-        {!loading &&
-          data?.findManyUser?.map((item) => {
-            return <div key={item.id}>{item.email}</div>;
-          })}
+              .then((data) => {
+                console.log(data);
+                refetch();
+                alert("user create");
+              })
+              .catch((e) => {
+                alert(e.message);
+              });
+          }}
+        >
+          Войти
+        </button>
+        {/*<p style={{paddingTop: 40}}>Пользователи</p>*/}
+        {/*<div style={{ marginTop: 20}}>*/}
+        {/*  {!loading &&*/}
+        {/*    data?.findManyUser?.map((item) => {*/}
+        {/*      return <div key={item.id}>{item.email}</div>;*/}
+        {/*    })}*/}
+        {/*</div>*/}
       </div>
+      <div className="mainbar downBar">
+        <button
+          className="textbox butIn butOut">
+          Зарегистрироваться
+        </button>
+        <p>После регистрации вы получите доступ ко всем возможностям нашей соц. сети</p>
+      </div>
+
     </div>
   );
 };
