a70b3e5f4b69d66dbbe4d1040d8fa12a79a24803
Aytaldeg <91862666+Aytaldeg@users.noreply.github.com>
2023-03-14 16:15:07 +0900

    Added pages, and fixed

---
+++ IsakovDegtyarev/client/src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/users" activeStyle>
+            Users
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
--- IsakovDegtyarev/client/src/index.js
+++ IsakovDegtyarev/client/src/index.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Header from './pages/header/Header';
-import Main from './pages/main/Main';
-import Footer from './pages/footer/Foote';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <div className='container'>
-    <Header />
-    <Main />
-    <Footer />
-    </div>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
