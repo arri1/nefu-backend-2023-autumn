9e93e368468c6c88a81dca72a2bbc3e146b8a45d
0dj3 <xaryshal@gmail.com>
2023-03-19 21:44:13 +0900

    Added getClientById function for tg-bot

--- InnokentievLukovtsev/server/src/tgBot/tgBot.js
+++ InnokentievLukovtsev/server/src/tgBot/tgBot.js
@@ -1,10 +1,22 @@
 const telegramBot = require('node-telegram-bot-api')
 const config = require('./config.json')
+const {createContext} = require("../context");
 
 const token = config.TOKEN
 const bot = new telegramBot(token, { polling: true })
 
-bot.on('message', msg => {
-    const {chat: {id}} = msg
-    bot.sendMessage(id,'Pong')
-})
+bot.onText(/\/r (.+)/, async (msg, match) => {
+    let context = createContext()
+    const chatId = msg.chat.id;
+    const response = match[1];
+    let cec = await context.prisma.User.findUnique({
+        where: {
+            id: Number(response)
+        }
+    });
+    const otv = "id: " + cec.id + "\nname: " + cec.name + "\nemail:" + cec.email;
+    bot.sendMessage(chatId,otv);
+    console.log(cec)
+});
+
+
