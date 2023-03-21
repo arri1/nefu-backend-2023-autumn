1a7b015b89e26817146d901d3181dab118f9729a
Alexey670412 <91104532+Alexey670412@users.noreply.github.com>
2023-03-21 15:21:28 +0900

    login

--- LotovFedorov/test-front-app/src/App.js
+++ LotovFedorov/test-front-app/src/App.js
@@ -1,5 +1,6 @@
-import { useQuery, gql, useMutation } from "@apollo/client";
+import { useQuery, gql, useMutation, useLazyQuery } from "@apollo/client";
 import { useState } from "react";
+import "./App.css";
 
 const FIND_MANY_USER = gql`
     query FindManyUser {
@@ -18,9 +19,20 @@ const CREATE_ONE_USER = gql`
     }
 `;
 
+const LOGIN_USER = gql`
+    query FindUniqueUser($where: UserWhereUniqueInput!) {
+        findUniqueUser(where: $where) {
+            email
+            password
+        }
+    }
+`;
+
 const App = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
 
     const { loading, error, data, refetch } = useQuery(FIND_MANY_USER);
     const [
@@ -32,6 +44,12 @@ const App = () => {
         },
     ] = useMutation(CREATE_ONE_USER);
     console.log(data);
+
+    const [
+        loginUser,
+        { loading: loginLoading, error: loginError, where: loginData },
+    ] = useLazyQuery(LOGIN_USER);
+
     return (
         <div
             style={{
@@ -41,44 +59,142 @@ const App = () => {
                 width: "100%",
                 flexDirection: "column",
                 height: "100vh",
+                background: "gray",
             }}
         >
-            <text style={{ marginTop: 15 }}>Email</text>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} />
-            <text style={{ marginTop: 15 }}>Password</text>
-            <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button
-                style={{ marginTop: 10, width: 100, height: 30 }}
-                onClick={() => {
-                    createUser({
-                        variables: {
-                            data: {
-                                email,
-                                password,
-                            },
-                        },
-                    })
-                        .then((data) => {
-                            console.log(data);
-                            refetch();
-                            alert("user create");
-                        })
-                        .catch((e) => {
-                            alert(e.message);
-                        });
-                }}
-            >
-                Registration
-            </button>
-                <text>Registred users</text>
-            <div style={{ marginTop: 10 }}>
-                {!loading &&
-                    data?.findManyUser?.map((item) => {
-                        return <div key={item.id}>{item.email}</div>;
-                    })}
+            <div className="header"></div>
+
+            <div className="main">
+                <div className="content">
+                    <div className="regLog">
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                width: "100%",
+                                flexDirection: "column",
+                            }}
+                        >
+                            <h1>Login</h1>
+                            <p style={{ marginTop: 15 }}>Email</p>
+                            <input
+                                value={loginEmail}
+                                onChange={(e) => setLoginEmail(e.target.value)}
+                            />
+                            <p style={{ marginTop: 15 }}>Password</p>
+                            <input
+                                value={loginPassword}
+                                onChange={(e) =>
+                                    setLoginPassword(e.target.value)
+                                }
+                            />
+                            <button
+                                style={{
+                                    marginTop: 10,
+                                    width: 100,
+                                    height: 30,
+                                }}
+                                onClick={() => {
+                                    loginUser({
+                                        variables: {
+                                            where: {
+                                                email: loginEmail,
+                                            },
+                                        },
+                                    })
+                                        .then((loginData) => {
+                                            if (
+                                                loginData.data.findUniqueUser
+                                                    .password === loginPassword
+                                            )
+                                                alert(
+                                                    "User successfully logined"
+                                                );
+                                            else alert("you are oshibka");
+                                        })
+                                        .catch((e) => console.log(e));
+                                }}
+                            >
+                                Login
+                            </button>
+                            <p>Registred users</p>
+                            <div style={{ marginTop: 10 }}>
+                                {!loading &&
+                                    data?.findManyUser?.map((item) => {
+                                        return (
+                                            <div key={item.id}>
+                                                {item.email}
+                                            </div>
+                                        );
+                                    })}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="regLog">
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                width: "100%",
+                                flexDirection: "column",
+                            }}
+                        >
+                            <h1>Register</h1>
+                            <p style={{ marginTop: 15 }}>Email</p>
+                            <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <p style={{ marginTop: 15 }}>Password</p>
+                            <input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button
+                                style={{
+                                    marginTop: 10,
+                                    width: 100,
+                                    height: 30,
+                                }}
+                                onClick={() => {
+                                    createUser({
+                                        variables: {
+                                            data: {
+                                                email,
+                                                password,
+                                            },
+                                        },
+                                    })
+                                        .then((data) => {
+                                            console.log(data);
+                                            refetch();
+                                            alert(
+                                                "User successfully registered"
+                                            );
+                                        })
+                                        .catch((e) => {
+                                            alert(e.message);
+                                        });
+                                }}
+                            >
+                                Registration
+                            </button>
+                            <p>Registred users</p>
+                            <div style={{ marginTop: 10 }}>
+                                {!loading &&
+                                    data?.findManyUser?.map((item) => {
+                                        return (
+                                            <div key={item.id}>
+                                                {item.email}
+                                            </div>
+                                        );
+                                    })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
